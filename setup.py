d5059beac8222ee5a0c2dc7d13c1cd232c7de217
Vincent Rabaud <vrabaud@willowgarage.com>
2012-10-22 07:51:30 -0700

    fix missing folders

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from distutils.core import setup
 setup(name='Object recognition core',
       version='1.0.0',
       description='The core of object recognition',
-      packages=['object_recognition_ros.io.ros', 'object_recognition_ros.io.ros.source',
+      packages=['object_recognition_ros', 'object_recognition_ros.io',
+                'object_recognition_ros.io.ros', 'object_recognition_ros.io.ros.source',
                 'object_recognition_ros.io.ros.sink'],
       package_dir={'':'python'}
 )
