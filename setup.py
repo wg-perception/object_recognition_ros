d308cbfc2c0bc104837bf02705ba4494373a2aa6
Vincent Rabaud <vrabaud@willowgarage.com>
2012-11-01 07:14:25 -0700

    get the information from the package.xml

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
-from distutils.core import setup
 
-setup(name='Object recognition modules for ROS',
-      version='1.0.0',
-      description='The ROS modules of object recognition',
-      packages=['object_recognition_ros', 'object_recognition_ros.io',
+from distutils.core import setup
+from ecto.package import parse_package_for_distutils
+
+d = parse_package_for_distutils()
+d['packages'] = ['object_recognition_ros', 'object_recognition_ros.io',
                 'object_recognition_ros.io', 'object_recognition_ros.io.source',
-                'object_recognition_ros.io.sink'],
-      package_dir={'':'python'}
-)
+                'object_recognition_ros.io.sink']
+d['package_dir'] = {'': 'python'}
+d['install_requires'] = []
+
+setup(**d)
