1c115d0cd45899726efbffcdc6c40d29d9847e7e
Vincent Rabaud <vrabaud@willowgarage.com>
2012-12-27 18:43:43 +0100

    comply to the new BlackBox API

--- python/object_recognition_ros/io/sink/publisher.py
+++ python/object_recognition_ros/io/sink/publisher.py
@@ -6,6 +6,7 @@ from object_recognition_core.boost.interface import ObjectDbParameters
 from object_recognition_ros.ecto_cells.io_ros import MsgAssembler, VisualizationMsgAssembler, Publisher_MarkerArray
 from object_recognition_core.io.sink import Sink
 import ecto
+from ecto import BlackBoxCellInfo, BlackBoxForward
 import ecto_ros.ecto_geometry_msgs as ecto_geometry_msgs
 import ecto_ros.ecto_std_msgs as ecto_std_msgs
 from object_recognition_msgs.ecto_cells.ecto_object_recognition_msgs import Publisher_RecognizedObjectArray
@@ -20,43 +21,42 @@ class PublisherBlackBox(ecto.BlackBox):
     """Class publishing the different results of object recognition as ROS topics
     http://ecto.willowgarage.com/releases/amoeba-beta3/ros/geometry_msgs.html#Publisher_PoseArray
     """
-    _pose_pub = PoseArrayPub
-    _marker_pub = MarkerArrayPub
-    _object_ids_pub = StringPub
-    passthrough = ecto.PassthroughN
-    _recognized_object_array = Publisher_RecognizedObjectArray
-    _msg_assembler = MsgAssembler()
-
     def __init__(self, do_visualize, **kwargs):
         self._do_visualize = do_visualize
         ecto.BlackBox.__init__(self, **kwargs)
 
-    def declare_params(self, p):
+    @classmethod
+    def declare_cells(cls, _p):
+        return {'msg_assembler': BlackBoxCellInfo(MsgAssembler)}
+
+    def declare_direct_params(self, p):
         p.declare('markers_topic', 'The ROS topic to use for the marker array.', 'markers')
         p.declare('pose_topic', 'The ROS topic to use for the pose array.', 'poses')
         p.declare('object_ids_topic', 'The ROS topic to use for the object meta info string', 'object_ids')
         p.declare('recognized_object_array_topic', 'The ROS topic to use for the recognized object', 'recognized_object_array')
         p.declare('latched', 'Determines if the topics will be latched.', True)
         p.declare('db_params', 'The DB parameters', ObjectDbParameters({}))
-        p.forward('publish_clusters', cell_name='_msg_assembler')
 
-    def declare_io(self, _p, i, _o):
-        i.forward_all('_msg_assembler')
+    def declare_forwards(self, _p):
+        p = {'msg_assembler': [BlackBoxForward('publish_clusters')]}
+        i = {'msg_assembler': 'all'}
+
+        return (p,i,{})
 
     def configure(self, p, _i, _o):
         self._recognized_object_array = Publisher_RecognizedObjectArray(topic_name=p.recognized_object_array_topic, latched=p.latched)
         if self._do_visualize:
             self._visualization_msg_assembler = VisualizationMsgAssembler()
-            self._pose_pub = PublisherBlackBox._pose_pub(topic_name=p.pose_topic, latched=p.latched)
-            self._object_ids_pub = PublisherBlackBox._object_ids_pub(topic_name=p.object_ids_topic, latched=p.latched)
-            self._marker_pub = PublisherBlackBox._marker_pub(topic_name=p.markers_topic, latched=p.latched)
+            self._pose_pub = PoseArrayPub(topic_name=p.pose_topic, latched=p.latched)
+            self._object_ids_pub = StringPub(topic_name=p.object_ids_topic, latched=p.latched)
+            self._marker_pub = MarkerArrayPub(topic_name=p.markers_topic, latched=p.latched)
 
-    def connections(self):
+    def connections(self, p):
         # connect to a publishing cell
-        connections = [ self._msg_assembler['msg'] >> self._recognized_object_array['input']]
+        connections = [ self.msg_assembler['msg'] >> self._recognized_object_array['input']]
 
         if self._do_visualize:
-            connections += [ self._msg_assembler['msg'] >> self._visualization_msg_assembler['msg'] ]
+            connections += [ self.msg_assembler['msg'] >> self._visualization_msg_assembler['msg'] ]
 
             connections += [self._visualization_msg_assembler['pose_message'] >> self._pose_pub[:],
                 self._visualization_msg_assembler['object_ids_message'] >> self._object_ids_pub[:],
