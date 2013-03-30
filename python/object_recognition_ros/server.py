ba1c598bcb59ea5bf7f4ce6dc80ef2e17b95de58
Vincent Rabaud <vincent.rabaud@gmail.com>
2013-03-30 13:28:52 +0000

    use 2to3 on the codebase

--- python/object_recognition_ros/server.py
+++ python/object_recognition_ros/server.py
@@ -58,7 +58,7 @@ class RecognitionServer:
 
         topics = ['recognized_object_array']
 
-        for sink in ork_params.itervalues():
+        for sink in ork_params.values():
             if 'recognized_object_array_topic' in sink:
                 topics.append(sink['recognized_object_array_topic'])
 
@@ -93,7 +93,7 @@ class RecognitionServer:
                     self.cropper.params.z_min = goal.filter_limits[4]
                     self.cropper.params.z_max = goal.filter_limits[5]
                 else:
-                    print >> sys.stderr, 'WARNING: goal.use_roi is enabled but filter_limits doesn\'t have size 6 [x_min, x_max, y_min, y_max, z_min, z_max]. Roi disabled.'
+                    print('WARNING: goal.use_roi is enabled but filter_limits doesn\'t have size 6 [x_min, x_max, y_min, y_max, z_min, z_max]. Roi disabled.', file=sys.stderr)
                     self.cropper.params.crop_enabled = False
 
         # Do lots of awesome groundbreaking robot stuff here
