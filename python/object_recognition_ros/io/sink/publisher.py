72d231a2acaabc36250e79b3761e778d96964ae3
Vincent Rabaud <vrabaud@willowgarage.com>
2013-10-20 21:00:10 +0200

    only publish official messages and visual them through the RViz plugin

--- python/object_recognition_ros/io/sink/publisher.py
+++ python/object_recognition_ros/io/sink/publisher.py
@@ -7,13 +7,12 @@ from object_recognition_core.boost.interface import ObjectDbParameters
 from object_recognition_core.io.sink import SinkBase
 from object_recognition_ros import init_ros
 from object_recognition_ros.ecto_cells.ecto_object_recognition_msgs import Publisher_RecognizedObjectArray
-from object_recognition_ros.ecto_cells.io_ros import MsgAssembler, VisualizationMsgAssembler, Publisher_MarkerArray
+from object_recognition_ros.ecto_cells.io_ros import MsgAssembler
 import ecto
 import ecto_ros.ecto_geometry_msgs as ecto_geometry_msgs
 import ecto_ros.ecto_std_msgs as ecto_std_msgs
 
 PoseArrayPub = ecto_geometry_msgs.Publisher_PoseArray
-MarkerArrayPub = Publisher_MarkerArray
 StringPub = ecto_std_msgs.Publisher_String
 
 ########################################################################################################################
@@ -32,8 +31,6 @@ class Publisher(ecto.BlackBox, SinkBase):
 
     @staticmethod
     def declare_direct_params(p):
-        p.declare('do_visualize', 'If True some markers are displayed for visualization. '
-                  'For RSO Groovy and above, use the RViz plugin instead.', False)
         p.declare('markers_topic', 'The ROS topic to use for the marker array.', 'markers')
         p.declare('pose_topic', 'The ROS topic to use for the pose array.', 'poses')
         p.declare('object_ids_topic', 'The ROS topic to use for the object meta info string', 'object_ids')
@@ -50,22 +47,9 @@ class Publisher(ecto.BlackBox, SinkBase):
 
     def configure(self, p, _i, _o):
         self._recognized_object_array = Publisher_RecognizedObjectArray(topic_name=p.recognized_object_array_topic, latched=p.latched)
-        if p.do_visualize:
-            self._visualization_msg_assembler = VisualizationMsgAssembler()
-            self._pose_pub = PoseArrayPub(topic_name=p.pose_topic, latched=p.latched)
-            self._object_ids_pub = StringPub(topic_name=p.object_ids_topic, latched=p.latched)
-            self._marker_pub = MarkerArrayPub(topic_name=p.markers_topic, latched=p.latched)
 
     def connections(self, p):
         # connect to a publishing cell
         connections = [ self.msg_assembler['msg'] >> self._recognized_object_array['input']]
-
-        if p.do_visualize:
-            connections += [ self.msg_assembler['msg'] >> self._visualization_msg_assembler['msg'] ]
-
-            connections += [self._visualization_msg_assembler['pose_message'] >> self._pose_pub[:],
-                self._visualization_msg_assembler['object_ids_message'] >> self._object_ids_pub[:],
-                self._visualization_msg_assembler['marker_message'] >> self._marker_pub[:]
-               ]
 
         return connections
